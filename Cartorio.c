2c84ae7e0c7a81a6d64b1ed2a79ccebda8e25299
Pedro Muller <pedro.paivamuller@gmail.com>
2024-07-02 15:37:17 -0300

    Opção de saída

--- Cartorio.c
+++ Cartorio.c
@@ -124,7 +124,8 @@
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n\n"); //fim do menu
-		printf("Opção: ");
+		printf("\t4 - Sair do Sistema\n\n");
+		printf("Opção: "); //fim do menu
 	
 		scanf("%d",&opcao); //armazenando a escolha do usuario
 	
@@ -142,6 +143,11 @@
 		
 		case 3:
 		deletar();
+		break;
+		
+		case 4:
+		printf("Obrigado por utilizar o sistema!");
+		return 0;
 		break;
 		
 		
